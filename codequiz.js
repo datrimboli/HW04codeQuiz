f32206b74ef4b582eb5ad90f4b379faed7e68b6b
Dave Trimboli <davidatrimboli@gmail.com>
2020-07-28 00:49:36 -0400

    html comments

--- codequiz.js
+++ codequiz.js
@@ -4,105 +4,123 @@ let questions = [
     // Question 1
     question: "",
     correct_answer: "",
-    answerchoices: ["", "", "", ""],
+    answerChoices: ["", "", "", ""],
   },
   {
     // Question 2
     question: "",
     correct_answer: "",
-    answerchoices: ["", "", "", ""],
+    answerChoices: ["", "", "", ""],
   },
   {
     // Question 3
     question: "",
     correct_answer: "",
-    answerchoices: ["", "", "", ""],
+    answerChoices: ["", "", "", ""],
   },
   {
     // Question 4
     question: "",
     correct_answer: "",
-    answerchoices: ["", "", "", ""],
+    answerChoices: ["", "", "", ""],
   },
   {
     // Question 5
     question: "",
     correct_answer: "",
-    answerchoices: ["", "", "", ""],
+    answerChoices: ["", "", "", ""],
   },
   {
     // Question 6
     question: "",
     correct_answer: "",
-    answerchoices: ["", "", "", ""],
+    answerChoices: ["", "", "", ""],
   },
   {
     // Question 7
     question: "",
     correct_answer: "",
-    answerchoices: ["", "", "", ""],
+    answerChoices: ["", "", "", ""],
   },
   {
     // Question 8
     question: "",
     correct_answer: "",
-    answerchoices: ["", "", "", ""],
+    answerChoices: ["", "", "", ""],
   },
   {
     // Question 9
     question: "",
     correct_answer: "",
-    answerchoices: ["", "", "", ""],
+    answerChoices: ["", "", "", ""],
   },
   {
     // Question 10
     question: "",
     correct_answer: "",
-    answerchoices: ["", "", "", ""],
+    answerChoices: ["", "", "", ""],
   },
 ];
-
+// Variable: Current Index. It moves us through questions array
 let currentIndex = 0;
+// Variable: Score. Score will increase with every right answer
 let score = 0;
-let seconds = 100;
+// Variable: Seconds. Seconds decrease as time continues
+let seconds = 90;
+// Variable: Timer. This will be the timer interval
 let timer;
 
+// newQuestion function will show the new questions
 const newQuestion = () => {
   document.getElementById("question").textContent =
     questions[currentIndex].question;
 
-  let answerchoices = questions[currentIndex].answerchoices;
+  // Variable: answerChoices. Stores answerChoices array for the question being answered
+  let answerChoices = questions[currentIndex].answerChoices;
 
-  document.getElementById("answerchoices").innerHTML = "";
+  // Resets buttons for each question
+  document.getElementById("answerChoices").innerHTML = "";
 
-  for (let i = 0; i < answerchoices.length; i++) {
+  // For loop that puts buttons on screen
+  for (let i = 0; i < answerChoices.length; i++) {
+    // Button element
     let answerElem = document.createElement("button");
+    // Styling for answer button
     answerElem.className = "answer btn btn-secondary btn-lg";
-    answerElem.dataset.answer = answerchoices[i];
-    answerElem.textContent = answerchoices[i];
-
-    document.getElementById("answerchoices").append(answerElem);
+    // Makes buttons value the answer and puts the text onto the button
+    answerElem.dataset.answer = answerChoices[i];
+    // text is equal to answer
+    answerElem.textContent = answerChoices[i];
+    // Pushes answer to answerElem
+    document.getElementById("answerChoices").append(answerElem);
   }
 };
-
+// Variable:Answer
 const getAnswer = (answer) => {
+  // Checks if answer was correct by seeing if answer selected and the correct answer match
   if (answer === questions[currentIndex].correct_answer) {
+    // Score goes up by single increments
     score++;
+    // shows score on screen
     document.getElementById("score").textContent = score;
+
     let resultElem = document.createElement("div");
     resultElem.className = "alert alert-success";
+    // Alerts correct answer
     resultElem.textContent = "Correct Answer";
-    document.getElementById("answerchoices").append(resultElem);
+    document.getElementById("answerChoices").append(resultElem);
   } else {
+    // Alerts incorrect answer
     let resultElem = document.createElement("div");
     resultElem.className = "alert alert-danger";
     resultElem.textContent = "Incorrect Answer";
-    document.getElementById("answerchoices").append(resultElem);
+    document.getElementById("answerChoices").append(resultElem);
   }
 
   currentIndex++;
-
+  // Timeout set
   setTimeout(() => {
+    // Determines how many questions are left
     if (currentIndex < questions.length) {
       newQuestion();
     } else {
@@ -111,7 +129,9 @@ const getAnswer = (answer) => {
   }, 1000);
 };
 
+// End of game screen
 const endGame = () => {
+  // Displays game over, final score, prompts for username, and submits username
   document.getElementById("quiz").innerHTML = `
     <h1 class="display-2">Game Over!</h1>
   <p class="display-4">Your final score is: ${score}</p>
@@ -127,19 +147,21 @@ const endGame = () => {
   `;
 };
 
+// Submits score to the leaderboard
 const submitScore = (submission) => {
   console.log(submission);
 
+  // Gets leaderboard array from local storage
   let leaderboard = JSON.parse(localStorage.getItem("leaderboard")) || [];
-
+  // Pushes submission to leaderboard array
   leaderboard.push(submission);
-
+  // Resets leaderboard
   localStorage.setItem("leaderboard", JSON.stringify(leaderboard));
-
+  // Scores show in decending order
   leaderboard.sort((a, b) => {
     return b.score - a.score;
   });
-
+  // Table of elements for leaderboard, assigns table class
   let tableElem = document.createElement("table");
   tableElem.className = "table";
   tableElem.innerHTML = `
@@ -151,42 +173,50 @@ const submitScore = (submission) => {
       </tr>
     </thead>
   `;
-
+  // Table body element
   let bodyElem = document.createElement("tbody");
-
+  // For loop for leaderboard array
   for (let i = 0; i < leaderboard.length; i++) {
+    // Creates new row elements for all usernames entered in leaderboard
     let rowElem = document.createElement("tr");
     rowElem.innerHTML = `
       <th scope="row">${i + 1}</th>
       <td>${leaderboard[i].username}</td>
       <td>${leaderboard[i].score}</td>
     `;
+    // Sends rowElem to bodyElem to be shown on screen
     bodyElem.append(rowElem);
   }
-
+  // Sends bodyElem to tableElem to be shown on screen
   tableElem.append(bodyElem);
-
+  // tableElem appended to main container
   document.getElementById("quiz").append(tableElem);
 };
-
+// Starts game when start button is clicked
 document.getElementById("startTrivia").addEventListener("click", () => {
+  // setIntercal function to timer
   timer = setInterval(() => {
+    // Decreses by 1 second intervals
     seconds--;
+    // New seconds values
     document.getElementById("time").textContent = seconds;
 
+    // If seconds are less than zero, endgame
     if (seconds <= 0) {
       clearInterval(timer);
       endGame();
     }
+    // 1000ms=1sec for the decreasing interval values
   }, 1000);
 
   newQuestion();
 });
-
+// Click events
 document.addEventListener("click", (event) => {
   if (event.target.classList.contains("answer")) {
     getAnswer(event.target.dataset.answer);
   } else if (event.target.id === "submitScore") {
+    // Stops what would normally happen, refreshing screen on end of game
     event.preventDefault();
     submitScore({
       username: document.getElementById("username").value,
